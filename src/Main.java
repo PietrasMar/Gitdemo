4737e1b7502a83127db09884900a8e5a6da3dd37
Pietras <pietra39@gmail.com>
2018-09-24 14:53:10 +0200

    fsfaf

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
+import static java.lang.System.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        out.println("Hello World!");
 
         int x = 5;
         int y = 10;
         int k = x*y;
 
-        System.out.println(k);
+        out.println(k + "dsadsad");
 
     }
 }

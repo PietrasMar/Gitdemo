d96f322c6b94a93e43b80696dc19aa7f19005f7b
Pietras <pietra39@gmail.com>
2018-09-24 13:31:11 +0200

    Obliczenia Rozmiaru

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int x = 5;
+        int y = 10;
+        int k = x*y;
+
+        System.out.println(k);
+
     }
 }
